52d5f264602b85ee8c2429c8b7511b42a3d9ce08
Dipraj Girase <girasedipraj52@gmail.com>
2025-01-27 12:57:04 +0530

    today mrng

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,43 @@ const { Router } = require("express")
 const adminRouter = Router();
 const {adminModel} = require("../db")
 
-adminRouter.post("/signup", function(req, res){      //ab userRouter bana chuke hai aur routing kiye hai toh we don't have access of app so userRouter use krenge uske jgah pe 
+// add three dependencies - bcrypt,zod,jwtTokens
+
+
+adminRouter.post("/signup", async function(req, res){      //ab userRouter bana chuke hai aur routing kiye hai toh we don't have access of app so userRouter use krenge uske jgah pe 
+    const { email, password, firstName, lastName } = req.body
+
+    await adminModel.create({
+        email,
+        password,
+        firstName,
+        lastName
+    })
+
     res.json({
         message: "Signup endpoint"
     })
 })
 
 adminRouter.post("/signin", function(req, res){
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+adminRouter.post("/course", function(req, res){
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+adminRouter.put("/course", function(req, res){
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+adminRouter.post("/course/bulk", function(req, res){
     res.json({
         message: "Signup endpoint"
     })
